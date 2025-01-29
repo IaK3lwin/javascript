4706f352a5773ee16bda9c0e2b1e30a8317d009f
IaK3lwin <53442885+IaK3lwin@users.noreply.github.com>
2025-01-29 11:23:32 -0300

    jss

---
+++ aula11/ex8.js
@@ -0,0 +1,9 @@
+var vel = 60.1
+console.log(`Você está andaddo a ${vel}Km/h `)
+
+if (vel > 60.0)
+{
+    console.log('você ultrapassou a velocidade permitida. você foi MULTADO!')
+}
+
+console.log('Dirija com cuidado! e use sinto de segurança')
