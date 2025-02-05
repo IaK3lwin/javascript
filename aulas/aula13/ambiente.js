9296becbc60e83d10956f0c296e60806f6cd100b
IaK3lwin <53442885+IaK3lwin@users.noreply.github.com>
2025-02-05 11:25:48 -0300

    aula sobre repetições

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,35 @@
+//estrutuas de repetições
+
+/* 
+
+digamos que queiremos fazer um algoritmo que rode
+um código de 500 vezes, concorda que seria chato fazer
+issoo manualmente? mas nãp se desespere! na linguagem de programações existe 
+uma estrutura maravilhosa chamadas de estrututas de repetição! 
+a primeira é chamada (while)
+
+*/
+
+
+var c = 1
+
+do
+{
+    //bloco do comando while
+    console.log(`o código passou por aqui ${c} vezes`)
+    //soma a variavel c para que chege a 6 e o loop acabe
+    c++
+} while (c <= 0)
+
+
+/*
+
+while (c <= 6)
+{
+    //bloco do comando while
+    console.log(`o código passou por aqui ${c} vezes`)
+    //soma a variavel c para que chege a 6 e o loop acabe
+    c++
+}
+
+*/
